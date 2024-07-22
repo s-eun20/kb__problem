50aad841ff7b261b3865c4e4486839aef6e82ae7
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2024-07-22 17:08:39 +0900

    Update DeptListUI.java

--- problem/0722/DeptListUI.java
+++ problem/0722/DeptListUI.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class DeptListUI {
     public static void main(String[] args) {
         DeptDAO deptDAO = new DeptDAO();
-        ArrayList<com.multi.q1.DeptVO> list = deptDAO.list();
+        ArrayList<DeptVO> list = deptDAO.list();
 
         for(DeptVO vo : list) {
             System.out.println(vo);
